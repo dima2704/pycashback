62bf459d1ef86008bc4c57a3c618f83dafc23d3e
student13 <student13@mail.ru>
2018-12-01 12:19:31 +0300

    Test cashback above limit

--- app/test/test_cashback.py
+++ app/test/test_cashback.py
@@ -1,3 +1,4 @@
+
 def cashback(amount):
     percent = 0.05
     limit = 3_000
@@ -5,3 +6,8 @@ def cashback(amount):
     if result > limit:
         return limit
     return result
+
+def test_cashback_above_limit():
+    resilt = cashback(1_000_000)
+    assert 3_000 == resilt
+
